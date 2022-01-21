3784bd87d89a442000ceb6eb419c58ddadc23f33
Piyush Singhania <piyushsinghania28@gmail.com>
2022-01-21 20:11:38 +0530

    Created suffled cards and turns

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 const cardImages= [
@@ -7,13 +8,28 @@ const cardImages= [
   { "src": "/img/scroll-1.png" },
   { "src": "/img/shield-1.png" },
   { "src": "/img/sword-1.png" },
-]
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // suffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id: Math.random()}));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  }
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
